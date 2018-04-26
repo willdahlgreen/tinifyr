6da6a254fdab7b9ee56750bcaf101b2eb945b112
willdahlgreen <will.dahlgreen@gmail.com>
2018-04-26 14:40:16 +0100

    first commit

---
+++ tinifyr.R
@@ -0,0 +1,23 @@
+tinifyr <- function(file, key){
+  
+  if (!require(pacman)) install.packages('pacman')
+  if (!require(jsonlite)) install.packages('jsonlite')
+  
+  library(pacman)
+  library(jsonlite)
+  
+  file <- paste0(dirname(file), "/", basename(file))
+  
+  request <- paste0("curl https://api.tinify.com/shrink --user api:", 
+                    key, 
+                    " --data-binary @", 
+                    file, 
+                    " --dump-header /dev/stdout")
+  
+  response <- system(request, intern = T)
+  
+  url <- jsonlite::fromJSON(response[9])$output$url
+  
+  download.file(url, paste0(dirname(file), "/optimised-", basename(file))) 
+  
+}
